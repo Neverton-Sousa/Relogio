d7e2729a655a6b556149f2becd6f07e3119d8b42
Neverton Sousa <nevertonsousa2@gmail.com>
2024-02-18 22:18:28 -0300

    novas

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,37 +22,30 @@ var body = document.getElementById("body");
 
 if (hr >= 6 && hr < 12) {
     body.classList.add("dia");
-    exibirMensagem();
+    exibirMensagem("Bom Dia");
     var relogioDivs = document.querySelectorAll('.relogio div');
     relogioDivs.forEach(function(elemento) {
     elemento.style.backgroundColor = "rgb(241, 253, 169)";
     elemento.style.color = "#595959";
     });
 
-} else {
-    body.classList.remove("dia");
-    var mensagem = document.getElementById("mensagem");
-    mensagem.style.display = "none";
-}
+} 
 
 if (hr >= 12 && hr < 18) {
     body.classList.add("tarde");
-    exibirMensagem1();
+    exibirMensagem("Boa Trade");
     var relogioDivs = document.querySelectorAll('.relogio div');
     relogioDivs.forEach(function(elemento) {
     elemento.style.backgroundColor = "rgb(63, 196, 114)";
     });
 
 
-} else {
-    body.classList.remove("tarde");
-    var mensagem1 = document.getElementById("mensagem1");
-    mensagem1.style.display = "none";
-}
+} 
 
 if ((hr >= 18 && hr < 24) || (hr >= 0 && hr < 6)) {
     body.classList.add("noite");
-    exibirMensagem2();
+    body.style.background = "linear-gradient(120deg, #ffe53bd8, #ff2525da)";
+    exibirMensagem("Boa Noite");
     var relogioDivs = document.querySelectorAll('.relogio div');
     relogioDivs.forEach(function(elemento) {
     elemento.style.backgroundColor = "gray";
@@ -60,24 +53,26 @@ if ((hr >= 18 && hr < 24) || (hr >= 0 && hr < 6)) {
     });
     
 
-} else {
-    body.classList.remove("noite");
-    var mensagem2 = document.getElementById("mensagem2");
-    mensagem2.style.display = "none";
-    }
+} 
 })
 
-function exibirMensagem() {
-    var mensagem = document.getElementById("mensagem");
-    mensagem.style.display = "block";
-}
+// function exibirMensagem() {
+//     var mensagem = document.getElementById("mensagem");
+//     mensagem.style.display = "block";
+    
 
-function exibirMensagem1() {
-    var mensagem1 = document.getElementById("mensagem1");
-    mensagem1.style.display = "block";
-}
+    
 
-function exibirMensagem2() {
-    var mensagem2 = document.getElementById("mensagem2");
+// }
+
+// function exibirMensagem1() {
+//     var mensagem1 = document.getElementById("mensagem1");
+//     mensagem1.style.display = "block";
+// }
+
+function exibirMensagem(saudacao) {
+    var mensagem2 = document.getElementById("mensagem");
     mensagem2.style.display = "block";
+    mensagem2.style.color = "#fdfdfdce";
+    mensagem2.innerHTML = saudacao + "!";
 }
